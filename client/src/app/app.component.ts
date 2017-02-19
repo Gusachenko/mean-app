4cff3d0c83b496f1344a5a53358ae70d057fe8cc
gusachenko <gusachenkogleb@gmail.com>
2017-02-19 22:35:38 +0300

    fix: module path

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,4 +4,6 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular App'; }
+export class AppComponent  { 
+  name: string = 'Angular App'; 
+}
