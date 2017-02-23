8e7ac918ae24723d63791ef717ab25cb04bf07c0
gusachenko <gusachenkogleb@gmail.com>
2017-02-23 22:09:49 +0300

    Angular Material Ready(without fonts)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+  <button md-raised-button>Raised button</button>
+  <a md-mini-fab routerLink="."><md-icon>check</md-icon></a>`,
 })
 export class AppComponent  { 
   name: string = 'Angular App!'; 
