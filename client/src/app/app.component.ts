3fb43717ef1a9b0a98c3ac74ebd0e4ee293c194b
gusachenko <gusachenkogleb@gmail.com>
2017-02-25 19:50:54 +0300

    Replace link's declarations. Add Roboto fonts.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>
   <button md-raised-button>Raised button</button>
-  <a md-mini-fab routerLink="."><md-icon>check</md-icon></a>`,
+  <a md-mini-fab routerLink="."><md-icon>check</md-icon></a>
+  <md-icon>pets</md-icon>`,
 })
 export class AppComponent  { 
   name: string = 'Angular App!'; 
